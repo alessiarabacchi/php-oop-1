90822db0602b454b1113a6a938b628386c7882df
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-03-04 15:36:03 +0100

    add Genre

---
+++ Models/Genre.php
@@ -0,0 +1,20 @@
+<?php
+
+class Genre {
+    private $name;
+    private $description;
+
+    public function __construct($name, $description) {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+}
+?>

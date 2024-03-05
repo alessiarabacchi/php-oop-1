6221ff4dac5c5b288bd8ee5f8d768d9fe9385ae0
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-03-05 13:47:20 +0100

    change index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
-$productions = include 'Data/db.php';
+require_once 'Models/Genre.php';
+require_once 'Models/Movie.php';
+require_once 'Models/TVSerie.php';
 
-foreach ($productions as $production) {
-    echo "Titolo: " . $production->getTitle() . "<br>";
-    echo "Lingua: " . $production->getLanguage() . "<br>";
-    echo "Voto: " . $production->getRating() . "<br>";
-    echo "Genere: " . $production->getGenre()->getName() . "<br><br>";
-}
+$genre = new Genre("Azione", "Film d'azione pieni di avventura e adrenalina.");
+
+$movie = new Movie("Film A", "Inglese", 8, $genre, 1000000, 120);
+$tvSerie = new TVSerie("Serie B", "Italiano", 7, $genre, 5);
+
+// Stampa le informazioni
+echo "Movie:<br>";
+echo "Titolo: " . $movie->getTitle() . "<br>";
+echo "Profitti: $" . $movie->getProfits() . "<br>";
+echo "Durata: " . $movie->getDuration() . " minuti<br><br>";
+
+echo "TV Serie:<br>";
+echo "Titolo: " . $tvSerie->getTitle() . "<br>";
+echo "Numero di stagioni: " . $tvSerie->getSeasons() . "<br>";
 ?>
